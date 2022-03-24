1af08a961492f5e13d1a3602bf816e306f74bfec
Nahuel <ncostamagna@digitalhouse.com>
2022-03-24 17:18:54 -0300

    initial project

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ncostamagna/axul_events_scheduler
+
+go 1.17
+
+require github.com/ncostamagna/alertzy-sdk v0.0.1
+
+require github.com/ncostamagna/streetflow v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/ncostamagna/alertzy-sdk/alertzy"
+	c "github.com/ncostamagna/streetflow/client"
+)
+
+const (
+	BASE_USER     = ""
+	BASE_CONTACT  = ""
+	USERNAME      = ""
+	PASSWORD      = ""
+	ALERTZY_TOKEN = ""
+)
+
+func main() {
+
+	var err error
+
+	id, token, err := userReq()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(id, token)
+
+	contacts, err := contReq(id, token)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(contacts)
+
+	cTrans := alertzy.NewClient("https://alertzy.app", ALERTZY_TOKEN)
+
+	for _, c := range contacts {
+		err = cTrans.Send(fmt.Sprintf("Hoy es el cumple de %s", c), fmt.Sprintf("Hoy es el cunple de %s acordate de saludarlo en su dia", c), alertzy.Critical, "birthday", "", "", nil)
+		fmt.Print(err)
+	}
+
+	os.Exit(0)
+
+}
+
+func userReq() (string, string, error) {
+
+	client := c.RequestBuilder{
+		Headers:        http.Header{},
+		BaseURL:        BASE_USER,
+		ConnectTimeout: 5000 * time.Millisecond,
+		LogTime:        true,
+	}
+
+	req := struct {
+		UserName string `json:"username"`
+		Password string `json:"password"`
+	}{USERNAME, PASSWORD}
+
+	reps := client.Post("/users/login", req)
+
+	if reps.Err != nil {
+		return "", "", reps.Err
+	}
+
+	if reps.StatusCode > 299 {
+		return "", "", fmt.Errorf("code: %d, message: %s", reps.StatusCode, reps)
+	}
+
+	res := struct {
+		Data struct {
+			User struct {
+				ID string `json:"id"`
+			} `json:"user"`
+			Token string `json:"token"`
+		} `json:"data"`
+	}{}
+
+	if err := json.Unmarshal(reps.Bytes(), &res); err != nil {
+		return "", "", err
+	}
+
+	return res.Data.User.ID, res.Data.Token, nil
+
+}
+
+func contReq(id, token string) ([]string, error) {
+
+	header := http.Header{}
+	header.Set("id", id)
+	header.Set("token", token)
+
+	client := c.RequestBuilder{
+		Headers:        header,
+		BaseURL:        BASE_CONTACT,
+		ConnectTimeout: 5000 * time.Millisecond,
+		LogTime:        true,
+	}
+
+	reps := client.Get("/contacts?birthday=39")
+
+	if reps.Err != nil {
+		return nil, reps.Err
+	}
+
+	if reps.StatusCode > 299 {
+		return nil, fmt.Errorf("code: %d, message: %s", reps.StatusCode, reps)
+	}
+
+	res := struct {
+		Data []struct {
+			FirstName string `json:"firstname"`
+			LastName  string `json:"lastname"`
+		} `json:"data"`
+	}{}
+
+	if err := json.Unmarshal(reps.Bytes(), &res); err != nil {
+		return nil, err
+	}
+
+	var contacts []string
+
+	for _, c := range res.Data {
+		contacts = append(contacts, fmt.Sprintf("%s %s", c.FirstName, c.LastName))
+	}
+
+	return contacts, nil
+
+}
